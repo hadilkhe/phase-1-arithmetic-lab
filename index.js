a2a4c7c58b3a7e2dd5025865bf0809d44259c7ea
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-09-13 11:41:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+//  Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+//  Random
+const random = Math.floor(Math.random() * 10) + 1;
+
+//  Modulo
+const num3 = 64;
+const num4 = 8;
+const mod = num3 % num4;
+
+//  Max
+const max = Math.max(1, 3, 5, 10, 20);
+
+// Testing the results
+console.log("Multiply:", multiply);
+console.log(" Random:", random);
+console.log("Modulo:", mod);
+console.log(" Max:", max);
